593c2fa4334da6035d04ee378bfe6bf8e81405c2
filip-svrcek-CN <svrcek@cngroup.dk>
2022-07-22 14:37:18 +0200

    cely card item klikatelny

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,16 +1,17 @@
 import { Card, CardBody, CardTitle, CardSubtitle, CardImg } from 'reactstrap';
-import { Link } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 
 import placeholder from '../images/food-placeholder.png';
+import { useState } from 'react';
 
 export function RecipeCard(props) {
   const { slug, title, preparationTime } = props;
+  const [navigateToDetail, setNavigateToDetail] = useState(false);
 
   return (
-    <Card className="h-100">
-      <Link to={`/recipe/${slug}`}>
-        <CardImg src={placeholder} alt="Preview" />
-      </Link>
+    <Card className="h-100" onClick={() => setNavigateToDetail(true)}>
+      {navigateToDetail && <Navigate to={`/recipe/${slug}`} />}
+      <CardImg src={placeholder} alt="Preview" />
       <CardBody>
         <CardTitle tag="h5">{title}</CardTitle>
         <CardSubtitle>{preparationTime} min</CardSubtitle>
