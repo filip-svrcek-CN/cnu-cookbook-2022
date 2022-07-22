a541691cf1f88b595e5bb97121d4676925709ba7
filip-svrcek-CN <svrcek@cngroup.dk>
2022-07-22 16:03:03 +0200

    recipeslist: hours+mins; sidedish

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,12 +1,23 @@
 import { Card, CardBody, CardTitle, CardSubtitle, CardImg } from 'reactstrap';
 import { Navigate } from 'react-router-dom';
+import { BiTimeFive, BiDish } from "react-icons/bi";
 
 import placeholder from '../images/food-placeholder.png';
 import { useState } from 'react';
 
 export function RecipeCard(props) {
-  const { slug, title, preparationTime } = props;
+  const { slug, title, preparationTime, sideDish } = props;
   const [navigateToDetail, setNavigateToDetail] = useState(false);
+
+  const hoursToPrepare = (prepTime) => {
+    const hours = Math.floor(prepTime / 60)
+    return hours > 0 ? hours : null;
+  }
+
+  const minsToPrepare = (prepTime) => {
+    const mins = Math.floor(prepTime % 60)
+    return mins > 0 ? mins : null;
+  }
 
   return (
     <Card className="h-100 recipeCard" onClick={() => setNavigateToDetail(true)}>
@@ -14,7 +25,12 @@ export function RecipeCard(props) {
       <CardImg src={placeholder} alt="Preview" />
       <CardBody>
         <CardTitle tag="h5">{title}</CardTitle>
-        <CardSubtitle>{preparationTime} min</CardSubtitle>
+        <CardSubtitle style={{ fontSize: "14px" }}>
+          {preparationTime && <BiTimeFive />}
+          {hoursToPrepare(preparationTime) && <span> {hoursToPrepare(preparationTime)} h</span>}
+          {minsToPrepare(preparationTime) && <span> {minsToPrepare(preparationTime)} min</span>}
+          {sideDish && <span> <BiDish /> {sideDish}</span>}
+        </CardSubtitle>
       </CardBody>
     </Card>
   )
