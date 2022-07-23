f030422d71b55203420906c4975b4f880a8f8fc0
filip-svrcek-CN <svrcek@cngroup.dk>
2022-07-23 13:15:49 +0200

    RecipeCardDetail component

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,23 +1,14 @@
-import { Card, CardBody, CardTitle, CardSubtitle, CardImg } from 'reactstrap';
+import { Card, CardBody, CardTitle, CardImg } from 'reactstrap';
 import { Navigate } from 'react-router-dom';
-import { BiTimeFive, BiDish } from "react-icons/bi";
 
 import placeholder from '../images/food-placeholder.png';
 import { useState } from 'react';
+import { RecipeCardDetail } from './RecipeCardDetail';
 
 export function RecipeCard(props) {
   const { slug, title, preparationTime, sideDish } = props;
   const [navigateToDetail, setNavigateToDetail] = useState(false);
 
-  const hoursToPrepare = (prepTime) => {
-    const hours = Math.floor(prepTime / 60)
-    return hours > 0 ? hours : null;
-  }
-
-  const minsToPrepare = (prepTime) => {
-    const mins = Math.floor(prepTime % 60)
-    return mins > 0 ? mins : null;
-  }
 
   return (
     <Card className="h-100 recipeCard" onClick={() => setNavigateToDetail(true)}>
@@ -25,12 +16,7 @@ export function RecipeCard(props) {
       <CardImg src={placeholder} alt="Preview" />
       <CardBody>
         <CardTitle tag="h5">{title}</CardTitle>
-        <CardSubtitle style={{ fontSize: "14px" }}>
-          {preparationTime && <BiTimeFive />}
-          {hoursToPrepare(preparationTime) && <span> {hoursToPrepare(preparationTime)} h</span>}
-          {minsToPrepare(preparationTime) && <span> {minsToPrepare(preparationTime)} min</span>}
-          {sideDish && <span> <BiDish /> {sideDish}</span>}
-        </CardSubtitle>
+        <RecipeCardDetail preparationTime={preparationTime} sideDish={sideDish} />
       </CardBody>
     </Card>
   )
---
+++ src/components/RecipeCardDetail.js
@@ -0,0 +1,32 @@
+import { CardSubtitle } from 'reactstrap';
+import { BiTimeFive, BiDish } from "react-icons/bi";
+
+export function RecipeCardDetail({ preparationTime, sideDish }) {
+
+  const hoursToPrepare = prepTime => {
+    const hours = Math.floor(prepTime / 60)
+    return hours > 0 ? hours : null;
+  }
+
+  const minsToPrepare = prepTime => {
+    const mins = Math.floor(prepTime % 60)
+    return prepTime === 0 ? "0" : mins > 0 ? mins : null;
+  }
+
+  if (preparationTime >= 0 || sideDish) {
+    return (
+      <CardSubtitle style={{ fontSize: "14px" }}>
+        {preparationTime >= 0 && <BiTimeFive />}
+        {hoursToPrepare(preparationTime) && <span> {hoursToPrepare(preparationTime)} h</span>}
+        {minsToPrepare(preparationTime) && <span> {minsToPrepare(preparationTime)} min</span>}
+        {sideDish && <span> <BiDish /> {sideDish}</span>}
+      </CardSubtitle>
+    )
+  }
+
+  return (
+    <CardSubtitle style={{ fontSize: "14px", fontStyle: "italic" }}>
+      žádné údaje
+    </CardSubtitle>
+  )
+}
