2b6343ea6c05f89994de8385508229a5580919d7
filip-svrcek-CN <svrcek@cngroup.dk>
2022-07-26 10:45:10 +0200

    recipecard link improvement

--- src/components/RecipeCard.js
+++ src/components/RecipeCard.js
@@ -1,28 +1,25 @@
 import { Card, CardBody, CardTitle, CardImg } from 'reactstrap';
-import { Navigate } from 'react-router-dom';
 
 import placeholder from '../images/food-placeholder.png';
-import { useState } from 'react';
 import { RecipeCardDetail } from './RecipeCardDetail';
 
 export function RecipeCard(props) {
   const { slug, title, preparationTime, sideDish } = props;
-  const [navigateToDetail, setNavigateToDetail] = useState(false);
 
   return (
-    <Card
-      className="h-100 recipeCard"
-      onClick={() => setNavigateToDetail(true)}
-    >
-      {navigateToDetail && <Navigate to={`/recipe/${slug}`} />}
-      <CardImg src={placeholder} alt="Preview" />
-      <CardBody>
-        <CardTitle tag="h5">{title}</CardTitle>
-        <RecipeCardDetail
-          preparationTime={preparationTime}
-          sideDish={sideDish}
-        />
-      </CardBody>
-    </Card>
+    <a href={`/recipe/${slug}`} style={{ textDecoration: "none", color: "#212529" }}>
+      <Card
+        className="h-100 recipeCard"
+      >
+        <CardImg src={placeholder} alt="Preview" />
+        <CardBody>
+          <CardTitle tag="h5">{title}</CardTitle>
+          <RecipeCardDetail
+            preparationTime={preparationTime}
+            sideDish={sideDish}
+          />
+        </CardBody>
+      </Card>
+    </a>
   );
 }
