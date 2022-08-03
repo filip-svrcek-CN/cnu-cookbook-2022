218483eb302891abeabde26dcc061490fcbf0f88
filip-svrcek-CN <svrcek@cngroup.dk>
2022-08-03 16:23:42 +0200

    directions MD guide link

--- src/components/DirectionsForm.js
+++ src/components/DirectionsForm.js
@@ -2,15 +2,28 @@ import { Input } from 'reactstrap';
 
 export function DirectionsForm({ updateData, directions }) {
   return (
-    <Input
-      onChange={(event) => {
-        const directions = event.target.value;
-        updateData({ directions });
-      }}
-      style={{ width: '100%' }}
-      type="textarea"
-      rows={20}
-      value={directions}
-    />
+    <div>
+      <Input
+        onChange={(event) => {
+          const directions = event.target.value;
+          updateData({ directions });
+        }}
+        style={{ width: '100%' }}
+        type="textarea"
+        rows={20}
+        value={directions}
+      />
+      <div
+        style={{ fontStyle: 'italic', fontSize: '12px', textAlign: 'right' }}
+      >
+        <a
+          style={{ color: 'black' }}
+          target="\_blank"
+          href="https://www.markdownguide.org/cheat-sheet/"
+        >
+          *Markdown syntax návod
+        </a>
+      </div>
+    </div>
   );
 }
